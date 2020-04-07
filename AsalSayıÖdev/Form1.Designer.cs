131ecb9476f7e55c9a183090af9e61c1cc790e76
gizem yıldız <yildizgizemnet@outlook.com>
2020-04-08 01:21:44 +0300

    ilk gönderi

---
+++ AsalSayıÖdev/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace AsalSayıÖdev
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1AsalSayi = new System.Windows.Forms.Button();
+            this.listBoxAsal = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // button1AsalSayi
+            // 
+            this.button1AsalSayi.Location = new System.Drawing.Point(504, 22);
+            this.button1AsalSayi.Name = "button1AsalSayi";
+            this.button1AsalSayi.Size = new System.Drawing.Size(97, 80);
+            this.button1AsalSayi.TabIndex = 0;
+            this.button1AsalSayi.Text = "Asal Sayılar";
+            this.button1AsalSayi.UseVisualStyleBackColor = true;
+            this.button1AsalSayi.Click += new System.EventHandler(this.button1AsalSayi_Click);
+            // 
+            // listBoxAsal
+            // 
+            this.listBoxAsal.FormattingEnabled = true;
+            this.listBoxAsal.Location = new System.Drawing.Point(12, 22);
+            this.listBoxAsal.Name = "listBoxAsal";
+            this.listBoxAsal.Size = new System.Drawing.Size(469, 381);
+            this.listBoxAsal.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBoxAsal);
+            this.Controls.Add(this.button1AsalSayi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1AsalSayi;
+        private System.Windows.Forms.ListBox listBoxAsal;
+    }
+}
+
---
+++ AsalSayıÖdev/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsalSayıÖdev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+            
+        }
+
+        private void button1AsalSayi_Click(object sender, EventArgs e)
+        {
+            int sayac = 0;
+            for (int i = 2; i <= 1000; i++)
+            {
+                int kontrol = 0;
+                for (int j = 2; j < i; j++)
+                {
+                    if (i%j ==0)
+                    {
+                        kontrol = 1;
+                        break;
+                    }
+                }
+                if (kontrol ==1)
+                {
+                    listBoxAsal.Items.Add(i + " asal değildir!");
+                }
+                else
+                {
+                    listBoxAsal.Items.Add(i + " asaldır.");
+                    sayac++;
+                }
+            }
+        }
+    }
+}
